50b3d7e4579fc079ce903a8034553ced0993ebf7
alanisFoz <alanis.foz.pr@gmail.com>
2020-06-19 19:27:47 -0300

    gran finale
    Signed-off-by: alanisFoz <alanis.foz.pr@gmail.com>

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,12 +7,32 @@ class Pessoa:
       self.filhos = list(filhos)
 
     def cumprimentar(self):
+        return f'Olá, meu nome é {self.nome}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+    def cumprimentar(self):
         return f'Óla {id(self)}'
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}.Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 
 
 if __name__ == '__main__':
-    chuck = Pessoa(nome='Chuck')
-    red = Pessoa(chuck, nome='Red')
+    chuck = Mutante(nome='Chuck')
+    red = Homem(chuck, nome='Red')
     print(Pessoa.cumprimentar(red))
     print(id(red))
     print(red.cumprimentar())
@@ -31,3 +51,13 @@ if __name__ == '__main__':
     print(red.olhos)
     print(chuck.olhos)
     print(id(Pessoa.olhos), id(red.olhos), id(Pessoa.olhos))
+    print(Pessoa.metodo_estatico(), red.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), red.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(chuck, Pessoa))
+    print(isinstance(chuck, Homem))
+    print(chuck.olhos)
+    print(red.cumprimentar())
+    print(chuck.cumprimentar())
